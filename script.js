62e9cb1eeeddd5cad351a34fd8d618f55e4a5162
fregart <fredrik@fredrikedman.se>
2026-08-17 11:58:42 +0200

    Create personal Javascript project

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = "Anna";
+const program = "JavaScript";
+const hobby = "musik";
+
+console.log(`Hej! Jag heter ${name}.`);
+console.log(`Jag studerar ${program}.`);
+console.log(`Mitt intresse är ${hobby}.`);
