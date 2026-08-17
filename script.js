e59d4dab7475928ec1d3f6069124eb2c179f22d6
fregart <fredrik@fredrikedman.se>
2026-08-17 13:00:23 +0200

    Add city information

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const name = "Anna";
 const program = "JavaScript";
 const hobby = "musik";
+const city = "musik";
 
 console.log(`Hej! Jag heter ${name}.`);
 console.log(`Jag studerar ${program}.`);
 console.log(`Mitt intresse är ${hobby}.`);
+console.log(`Jag bor i ${city}.`);
 
 function introduce(name, program, hobby) {
  return `Hej! Jag heter ${name}, studerar ${program} och gillar ${hobby}.`;
